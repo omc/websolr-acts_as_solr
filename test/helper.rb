8bea113cb0a779868f6bc17eb39c46675dd83a93
Kyle Maxwell <kyle@kylemaxwell.com>
2010-01-21 14:25:04 -0800

    Initial commit to websolr-acts_as_solr.

---
+++ test/helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'websolr-acts_as_solr'
+
+class Test::Unit::TestCase
+end
